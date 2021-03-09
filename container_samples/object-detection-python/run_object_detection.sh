72d83f3939044e384a80013b72052b59bf0c1115
Mishra <farida.mishra@intel.com>
2021-03-08 17:10:41 -0800

    Object detection container sample

--- container_samples/object-detection-python/run_object_detection.sh
+++ container_samples/object-detection-python/run_object_detection.sh
@@ -1,14 +1,23 @@
-mkdir -p  /opt/intel/openvino_2021.2.185/python/samples/models/retinanet-tf/FP16
-python3 /opt/intel/openvino_2021.2.185/deployment_tools/tools/model_downloader/downloader.py  --name retinanet-tf -o raw_models 
-python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo.py \
---input_model raw_models/public/retinanet-tf/retinanet_resnet50_coco_best_v2.1.0.pb \
---input_shape [1,1333,1333,3] \
---input input_1 \
---mean_values [103.939,116.779,123.68] \
---output filtered_detections/map/TensorArrayStack/TensorArrayGatherV3,filtered_detections/map/TensorArrayStack_1/TensorArrayGatherV3,filtered_detections/map/TensorArrayStack_2/TensorArrayGatherV3 \
---transformations_config /opt/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/retinanet.json \
---output_dir /opt/intel/openvino_2021.2.185/python/samples/models/retinanet-tf/FP16 \
+python3 /opt/intel/openvino_2021.2.185/deployment_tools/tools/model_downloader/downloader.py  --name mobilenet-ssd -o raw_models 
+
+mkdir -p models/mobilenet-ssd/FP16
+mkdir -p models/mobilenet-ssd/FP32
+
+
+# Create FP16 IR files
+python3 /opt/intel/openvino_2021.2.185/deployment_tools/model_optimizer/mo.py \
+--input_model raw_models/public/mobilenet-ssd/mobilenet-ssd.caffemodel \
 --data_type FP16 \
---model_name retinanet-tf
-python3 /opt/intel/openvino_2021.2.185/python/samples/object_detection.py -i cars_1900_first_frame.jpg  -m  /opt/intel/openvino_2021.2.185/python/samples/models/retinanet-tf/FP16/retinanet-tf.xml  --labels labels.txt -o ./
+--output_dir models/mobilenet-ssd/FP16 \
+--scale 256 \
+--mean_values [127,127,127]
+
+# Create FP32 IR files
+python3 /opt/intel/openvino_2021.2.185/deployment_tools/model_optimizer/mo.py \
+--input_model raw_models/public/mobilenet-ssd/mobilenet-ssd.caffemodel \
+--data_type FP32 \
+--output_dir models/mobilenet-ssd/FP32 \
+--scale 256 \
+--mean_values [127,127,127]
+
 
