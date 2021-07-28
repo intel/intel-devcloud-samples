31c2879445fbaf744404ee70a75a4704a5dd5525
Ojas D Sawant <ojas.d.sawant@intel.com>
2021-07-28 07:54:28 -0700

    containerized dlstreamer tutorial

---
+++ container-workloads/openvino-dev-latest/tutorials/python/dlstreamer/scripts/build_dlstreamer.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d735fdea07d537f931633273f43f1f274f147823f40215aa2ced0011977c465
+size 156
---
+++ container-workloads/openvino-dev-latest/tutorials/python/dlstreamer/vehicle_detection_benchmark.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d26f90a79be5b9334b12d9f84d1e52275fb644cb728bf046dbde512b1cab611
+size 1292
