331d9e4914e602e237879aabb94ba86060420d90
Farida Mishra <farida.mishra@intel.com>
2021-09-14 04:30:22 +0530

    Updated 2021.4 scripts

---
+++ container-workloads/openvino-dev-latest/developer-samples/python/pneumonia-classification/scripts/build_2021.4.sh
@@ -0,0 +1,2 @@
+sudo docker build   -t pneumnia_classification_2021.4 -f ./developer-samples/python/pneumonia-classification/dockerfile/ubuntu18/openvino_cgvh_dev_2021.4.dockerfile  .
+
--- container-workloads/openvino-dev-latest/developer-samples/python/pneumonia-classification/scripts/run_pnue_class_latest.sh
+++
@@ -1,2 +0,0 @@
-#sudo docker run  --env-file conftest1.env pneumonia-classification_2021.3:latest
-sudo docker run  --env-file conftest.env pneumonia-classification_2021.3:latest
