b0d6e6c967df263bdd188f71b9c93d26dcce4330
Farida Mishra <farida.mishra@intel.com>
2021-09-27 23:59:45 +0530

    Added colon for Latency column

--- container-workloads/openvino-dev-latest/developer-samples/python/tiny-yolo-v3/scripts/run_tiny-yolo-v3.sh
+++ container-workloads/openvino-dev-latest/developer-samples/python/tiny-yolo-v3/scripts/run_tiny-yolo-v3.sh
@@ -1,5 +1,2 @@
-#sudo docker run  -e $RUN_ON_PREM=data -it object_detection_2021.4:latest
-sudo docker run  -e RUN_ON_PREM=data tiny-yolo-v3_2021.4:latest
-
-
+sudo docker run -e RUN_ON_PREM=data -it tiny-yolo-v3_2021.4:latest
 
