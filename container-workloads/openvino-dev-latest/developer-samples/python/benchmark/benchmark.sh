b8d5cd69a1971af1419e504642b8f86b1c4cdd63
Prem Rao <premchander.rao.t@intel.com>
2023-09-13 17:13:51 +0530

    Update benchmark.sh

--- container-workloads/openvino-dev-latest/developer-samples/python/benchmark/benchmark.sh
+++ container-workloads/openvino-dev-latest/developer-samples/python/benchmark/benchmark.sh
@@ -16,14 +16,13 @@ Output_folder_16="$RUN_ON_PREM/$OUTPUT_FOLDER/results/FP16"
 XML_IR_FP16="$RUN_ON_PREM/$OUTPUT_FOLDER/IR/FP16"
 XML_IR_FP32="$RUN_ON_PREM/$OUTPUT_FOLDER/IR/FP32"
 
-IR_FP16="$XML_IR_FP16/mobilenet-ssd.xml"
-IR_FP32="$XML_IR_FP32/mobilenet-ssd.xml"
 
 sample_name="benchmark"
 
-MODEL="resnet-50-tf"
+MODEL="face-detection-retail-0005"
 source /opt/intel/openvino_$OPENVINO_VERSION/setupvars.sh 
 mkdir -p $RUN_ON_PREM/models
+#chmod -R 777 $RUN_ON_PREM
 omz_downloader  --name $MODEL  -o $RUN_ON_PREM/models
 
 
@@ -32,15 +31,11 @@ then
    echo "Creating output folder \$FP16"
    mkdir -p $Output_folder_16
    mkdir -p $XML_IR_FP16
-   mo \
-   --compress_to_fp16 \
-   --input_model $RUN_ON_PREM/models/public/resnet-50-tf/resnet_v1-50.pb  \
-   --input_shape=[1,224,224,3] \
-   --mean_values=[123.68,116.78,103.94] \
-   --output_dir $XML_IR_FP16 \
+   
+   cp $RUN_ON_PREM/models/intel/$MODEL/FP16/* $XML_IR_FP16
+   
 
-
-   python3 /opt/intel/openvino_$OPENVINO_VERSION/python/samples/benchmark/benchmark_app.py -m $XML_IR_FP16/resnet_v1-50.xml \
+   python3 /opt/intel/openvino_$OPENVINO_VERSION/python/samples/benchmark/benchmark_app.py -m $XML_IR_FP16/$MODEL.xml \
             -d $DEVICE \
             -niter 10 \
             -api $API \
@@ -50,4 +45,3 @@ then
    cat $RUN_ON_PREM/$OUTPUT_FOLDER/results/FP16/performance.txt
 
 fi
-
